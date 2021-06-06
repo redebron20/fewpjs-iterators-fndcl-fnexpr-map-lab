593d90c8fbdb3044fa0c559b187665f31bd8050e
redebron20 <redebron20@gmail.com>
2021-06-06 14:58:21 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (input) => {
+  return tutorials.map(title => {
+    const words = title.split(' ');
+    const capitalizedWords =
+      words.map( word => word.charAt(0).toUpperCase() + word.slice(1) );
+    const result = capitalizedWords.join(' ');
+    return result;
+  })
 }
